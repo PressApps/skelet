3f0a0809c10ee07f226ea1ea6affda13d832803b
Champ Camba <champ.kazban25@gmail.com>
2015-06-02 01:18:28 +0800

    Initial progress

---
+++ skelet.php
@@ -0,0 +1,45 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+/**
+ * Skelet Framework
+ * @version 1.0.0 
+ * @author pressapps <support@pressapps.co>
+ * 
+ */
+// ------------------------------------------------------------------------------------------------
+include_once dirname( __FILE__ ) .'/path.php';
+// ------------------------------------------------------------------------------------------------
+
+if( ! function_exists( 'cs_framework_init' ) && ! class_exists( 'CSFramework' ) ) {
+  function cs_framework_init() {
+
+    // active modules
+    defined( 'CS_ACTIVE_FRAMEWORK' )  or  define( 'CS_ACTIVE_FRAMEWORK',  true );
+    defined( 'CS_ACTIVE_METABOX'   )  or  define( 'CS_ACTIVE_METABOX',    true );
+    defined( 'CS_ACTIVE_SHORTCODE' )  or  define( 'CS_ACTIVE_SHORTCODE',  true );
+    defined( 'CS_ACTIVE_CUSTOMIZE' )  or  define( 'CS_ACTIVE_CUSTOMIZE',  true );
+
+    // helpers
+    cs_locate_template ( 'functions/deprecated.php'     );
+    cs_locate_template ( 'functions/helpers.php'        );
+    cs_locate_template ( 'functions/actions.php'        );
+    cs_locate_template ( 'functions/enqueue.php'        );
+    cs_locate_template ( 'functions/sanitize.php'       );
+    cs_locate_template ( 'functions/validate.php'       );
+
+    // classes
+    cs_locate_template ( 'classes/abstract.class.php'   );
+    cs_locate_template ( 'classes/options.class.php'    );
+    cs_locate_template ( 'classes/framework.class.php'  );
+    cs_locate_template ( 'classes/metabox.class.php'    );
+    cs_locate_template ( 'classes/shortcode.class.php'  );
+    cs_locate_template ( 'classes/customize.class.php'  );
+
+    // configs
+    cs_locate_template ( '../../includes/admin/options/framework.config.php'  );
+    cs_locate_template ( '../../includes/admin/options/metabox.config.php'    );
+    cs_locate_template ( '../../includes/admin/options/shortcode.config.php'  );
+    cs_locate_template ( '../../includes/admin/options/customize.config.php'  );
+    
+  }
+  add_action( 'init', 'cs_framework_init', 10 );
+}
