ad959baf958429173b97bff03bf3e9d646d70ebb
Champ Camba <champ.kazban25@gmail.com>
2015-06-16 20:07:58 +0800

    Add template hooks and pre process filters

--- classes/template.class.php
+++ classes/template.class.php
@@ -9,8 +9,27 @@
  */
 if(!class_exists("SkeletFramework_Template")){
   class SkeletFramework_Template {
+  		function __construct(){
+  			add_filter("template_include",array($this,"template_include"));
+  			add_filter('pre_get_posts',	  array($this,'pre_get_posts'   ));
+  		}
   		public static function instance($options = array()){
+  				new self();
   				//var_dump($options);
+  		}
+
+  		public function template_include($template){
+  			global $wp_query, $post;
+
+  				
+  				return $template;
+  		}
+
+  		public function pre_get_posts($query){
+
+  				//var_dump($query);
+
+  				return $query;
   		}
   }
 }
