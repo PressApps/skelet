691380aa263fd1c92570da9311921c79ed372ae1
Champ Camba <champ.kazban25@gmail.com>
2015-06-17 22:31:14 +0800

    Add post types

--- classes/abstract.class.php
+++ classes/abstract.class.php
@@ -21,5 +21,38 @@ if(!class_exists("SkeletFramework_Abstract")){
 	    add_action( $tag, array( &$this, $function_to_add), $priority, $accepted_args );
 	  }
 
+	  public function post_types(){
+	  	 
+	  	$arr_post_types = array(
+	  			 'is_single',
+				 'is_preview',
+				 'is_page',
+				 'is_archive',
+				 'is_date',
+				 'is_year',
+				 'is_month',
+				 'is_day',
+				 'is_time',
+				 'is_author',
+				 'is_category',
+				 'is_tag',
+				 'is_tax',
+				 'is_search',
+				 'is_feed',
+				 'is_comment_feed',
+				 'is_trackback',
+				 'is_home',
+				 'is_404',
+				 'is_comments_popup',
+				 'is_paged',
+				 'is_admin',
+				 'is_attachment',
+				 'is_singular',
+				 'is_robots',
+				 'is_posts_page',
+				 'is_post_type_archive');
+	  	return $arr_post_types;
+	  }
+
 	}
 }
