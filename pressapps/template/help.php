069a8e7ee597472c15a808642344dc50e4c9ed7f
Ivan Chromjak <ivan@guerilla.io>
2015-07-24 00:18:27 +0200

    help wip

--- pressapps/template/help.php
+++ pressapps/template/help.php
@@ -8,7 +8,7 @@
 
 <header class="pa-header">
 	<h1><?php _e( 'How can we help?', SK_TEXTDOMAIN ); ?></h1>
-	<h3>Ask a question or look for further help below.</h3>
+	<h3>Ask a question or look for further help below</h3>
 </header>
 
 <ul class="pa-boxes">
@@ -16,51 +16,38 @@
 		<a href="#">
 			<i class="sk-icon fa fa-file-text-o"></i>
 			<h3><?php _e( 'Documentation', SK_TEXTDOMAIN ); ?></h3>
-			<p>Learn how to install, setup and use our products, documentation is bundled with all our premioum products</p>
+			<p>Learn how to install, setup and use our products</p>
 		</a>
 	</li>
 	<li>
 		<a href="#">
 			<i class="sk-icon fa fa-book"></i>
 			<h3><?php _e( 'Knowledge Base', SK_TEXTDOMAIN ); ?></h3>
-			<p>Articles and screencast on setting up and configuring our products with advanced customisation advise</p>
-		</a>
-	</li>
-	<li>
-		<a href="#">
-			<i class="sk-icon fa fa-code"></i>
-			<h3><?php _e( 'Customization', SK_TEXTDOMAIN ); ?></h3>
-			<p>Do you want to customise our product but don't have the time or skills? Let us help we know our products best.</p>
+			<p>Search articles and screencast with customisation advise</p>
 		</a>
 	</li>
 </ul>
 
+<h2 class="pa-subheader"><?php _e( 'Looking for More?', SK_TEXTDOMAIN ); ?></h2>
 <ul class="pa-boxes">
 	<li>
 		<a href="#">
 			<i class="sk-icon fa fa-life-ring"></i>
-			<h3><?php _e( 'Pro Support', SK_TEXTDOMAIN ); ?></h3>
-			<p>Learn how to install, setup and use our products, documentation is bundled with all our premioum products</p>
-		</a>
-	</li>
-	<li>
-		<a href="#">
-			<i class="sk-icon fa fa-wrench"></i>
-			<h3><?php _e( 'Instalation and Setup', SK_TEXTDOMAIN ); ?></h3>
-			<p>Articles and screencast on setting up and configuring our products with advanced customisation advise</p>
+			<h3><?php _e( 'Priority Support', SK_TEXTDOMAIN ); ?></h3>
+			<p>Sign up to always be at the front of the queue and get rapid support</p>
 		</a>
 	</li>
 	<li>
 		<a href="#">
 			<i class="sk-icon fa fa-code"></i>
 			<h3><?php _e( 'Customization', SK_TEXTDOMAIN ); ?></h3>
-			<p>Do you want to customise our product but don't have the time or skills? Let us help we know our products best.</p>
+			<p>Do you want to customise our product and don't have the time or skills</p>
 		</a>
 	</li>
 </ul>
 
 <div class="pa-faq">
-    <h2>Frequently Asked Questions</h2>
+	<h2 class="pa-subheader"><?php _e( 'Frequently Asked Questions', SK_TEXTDOMAIN ); ?></h2>
 	<ul>
 		<li>
 			<h4 class="pa-question">Instalation and Setup</h4>
@@ -118,14 +105,14 @@
 </div>
 
 <div class="pa-cta">
-	<h2>Can’t find what you’re looking for?</h2>
+	<h2 class="pa-subheader"><?php _e( 'Can’t find what you’re looking for?', SK_TEXTDOMAIN ); ?></h2>
 	<p>We offer ticket support Monday – Friday between the hours of 7am and 3pm GMT time.</p>
 	<p>We reply to every message we receive within 24 hours Monday – Friday. Use our site to create a support ticket, and get help you can read whenever is most convenient.</p>
 	<a class="btn-cta" href="#">Open A Ticket</a>
 </div>
 
 <div class="pa-feedback">
-	<h2>Let Us Know What You Think</h2>
+	<h2 class="pa-subheader"><?php _e( 'Let Us Know What You Think', SK_TEXTDOMAIN ); ?></h2>
 	<p>Your feedback helps us build better products and create better experience for you. Please share you your thoughts and ideas.</p>
-	<a class="btn-cta" href="#">Feedback</a>
+	<a class="btn-cta" target="_blank" href="http://pressapps.co/feedback/">Feedback</a>
 </div>
