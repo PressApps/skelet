b1781f72f1b2e2082c3a38b907afe5a320116660
Ivan Chromjak <ivan@guerilla.io>
2015-07-24 14:09:36 +0200

    help button, text, links

--- pressapps/template/help.php
+++ pressapps/template/help.php
@@ -8,19 +8,19 @@
 
 <header class="pa-header">
 	<h1><?php _e( 'How can we help?', SK_TEXTDOMAIN ); ?></h1>
-	<h3>Ask a question or look for further help below</h3>
+	<p>Look for help below, get premium services or ask a question</p>
 </header>
 
 <ul class="pa-boxes">
 	<li>
-		<a href="#">
+		<a href="http://pressapps.co/docs/">
 			<i class="sk-icon fa fa-file-text-o"></i>
 			<h3><?php _e( 'Documentation', SK_TEXTDOMAIN ); ?></h3>
 			<p>Learn how to install, setup and use our products</p>
 		</a>
 	</li>
 	<li>
-		<a href="#">
+		<a href="http://help.pressapps.co/">
 			<i class="sk-icon fa fa-search"></i>
 			<h3><?php _e( 'Knowledge Base', SK_TEXTDOMAIN ); ?></h3>
 			<p>Search articles and screencast with customisation advise</p>
@@ -31,14 +31,14 @@
 <h2 class="pa-subheader"><?php _e( 'Looking for more?', SK_TEXTDOMAIN ); ?></h2>
 <ul class="pa-boxes">
 	<li>
-		<a href="#">
+		<a href="http://pressapps.co/priority/">
 			<i class="sk-icon fa fa-life-ring"></i>
 			<h3><?php _e( 'Priority Support', SK_TEXTDOMAIN ); ?></h3>
 			<p>Sign up to always be at the front of the queue and get rapid support</p>
 		</a>
 	</li>
 	<li>
-		<a href="#">
+		<a href="http://pressapps.co/customization/">
 			<i class="sk-icon fa fa-code"></i>
 			<h3><?php _e( 'Customization', SK_TEXTDOMAIN ); ?></h3>
 			<p>Do you want to customise our product and don't have the time or skills</p>
