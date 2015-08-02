6af204e246eedf4d8f22e1ad98c150cb892de579
Ivan Chromjak <ivan@guerilla.io>
2015-08-02 15:55:35 +0200

    help page update

--- pressapps/template/help.php
+++ pressapps/template/help.php
@@ -13,17 +13,17 @@
 
 <ul class="pa-boxes">
 	<li>
-		<a href="http://pressapps.co/docs/">
+		<a href="http://docs.pressapps.co/">
 			<i class="sk-icon fa fa-file-text-o"></i>
 			<h3><?php _e( 'Documentation', SK_TEXTDOMAIN ); ?></h3>
-			<p>Learn how to install, setup and use our products</p>
+			<p>Learn how to install, setup, use and customize our products</p>
 		</a>
 	</li>
 	<li>
-		<a href="http://help.pressapps.co/">
-			<i class="sk-icon fa fa-search"></i>
-			<h3><?php _e( 'Knowledge Base', SK_TEXTDOMAIN ); ?></h3>
-			<p>Search articles and screencast with customisation advise</p>
+		<a href="http://pressapps.co/support/">
+			<i class="sk-icon fa fa-life-ring"></i>
+			<h3><?php _e( 'Standard Support', SK_TEXTDOMAIN ); ?></h3>
+			<p>Envato market purchases include six month of complimentary standard support</p>
 		</a>
 	</li>
 </ul>
@@ -31,24 +31,24 @@
 <h2 class="pa-subheader"><?php _e( 'Looking for more?', SK_TEXTDOMAIN ); ?></h2>
 <ul class="pa-boxes">
 	<li>
-		<a href="http://pressapps.co/priority/">
+		<a href="http://pressapps.co/premium-support/">
 			<i class="sk-icon fa fa-life-ring"></i>
-			<h3><?php _e( 'Priority Support', SK_TEXTDOMAIN ); ?></h3>
-			<p>Sign up to always be at the front of the queue and get rapid support</p>
+			<h3><?php _e( 'Premium Support', SK_TEXTDOMAIN ); ?></h3>
+			<p>Let us help you with rapid response times and advanced technical support</p>
 		</a>
 	</li>
 	<li>
-		<a href="http://pressapps.co/customization/">
+		<a href="mailto:customization@pressapps.co">
 			<i class="sk-icon fa fa-code"></i>
 			<h3><?php _e( 'Customization', SK_TEXTDOMAIN ); ?></h3>
-			<p>Do you want to customise our product and don't have the time or skills</p>
+			<p>Do you want to customise our product and don't have the time or skills, get in touch</p>
 		</a>
 	</li>
 </ul>
 
 <div class="pa-cta">
 	<h2 class="pa-subheader"><?php _e( 'Can’t find what you’re looking for?', SK_TEXTDOMAIN ); ?></h2>
-	<p>All our products include 6 month of basic support answering functionality and setup queries. We reply to queries within 48 hours Monday to Friday.</p>
+	<p>All our products include six month of standard support answering functionality and setup queries. We reply to queries within 24 hours Monday to Friday.</p>
 	<a class="btn-cta" target="_blank" href="http://pressapps.co/support/"><?php _e( 'Open A Ticket', SK_TEXTDOMAIN ); ?></a>
 </div>
 
