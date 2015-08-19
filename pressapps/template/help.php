335baefba39a33972658b9953c82ed4d386b650f
Ivan Chromjak <ivan@guerilla.io>
2015-08-19 13:48:22 +0200

    help page update

--- pressapps/template/help.php
+++ pressapps/template/help.php
@@ -8,7 +8,7 @@
 
 <header class="pa-header">
 	<h1><?php _e( 'How can we help?', SK_TEXTDOMAIN ); ?></h1>
-	<p>Look for help below, get premium services or ask a question</p>
+	<p><?php _e( 'Find answers in documentation or contact us through standard or premium support', SK_TEXTDOMAIN ); ?></p>
 </header>
 
 <ul class="pa-boxes">
@@ -16,44 +16,38 @@
 		<a href="http://docs.pressapps.co/">
 			<i class="sk-icon fa fa-file-text-o"></i>
 			<h3><?php _e( 'Documentation', SK_TEXTDOMAIN ); ?></h3>
-			<p>Learn how to install, setup, use and customize our products</p>
+			<p><?php _e( 'Learn how to install, setup, use and customize our products in documentation', SK_TEXTDOMAIN ); ?></p>
 		</a>
 	</li>
 	<li>
 		<a href="http://pressapps.co/support/">
 			<i class="sk-icon fa fa-life-ring"></i>
 			<h3><?php _e( 'Standard Support', SK_TEXTDOMAIN ); ?></h3>
-			<p>Envato market purchases include six month of complimentary standard support</p>
+			<p><?php _e( 'Envato market purchases include six month of complimentary standard support', SK_TEXTDOMAIN ); ?></p>
+		</a>
+	</li>
+	<li>
+		<a href="http://pressapps.co/premium-support/">
+			<i class="sk-icon fa fa-star-o"></i>
+			<h3><?php _e( 'Premium Support', SK_TEXTDOMAIN ); ?></h3>
+			<p><?php _e( 'Let us help you with rapid response times and advanced technical support', SK_TEXTDOMAIN ); ?></p>
 		</a>
 	</li>
 </ul>
 
 <h2 class="pa-subheader"><?php _e( 'Looking for more?', SK_TEXTDOMAIN ); ?></h2>
-<ul class="pa-boxes">
-	<li>
-		<a href="http://pressapps.co/premium-support/">
-			<i class="sk-icon fa fa-life-ring"></i>
-			<h3><?php _e( 'Premium Support', SK_TEXTDOMAIN ); ?></h3>
-			<p>Let us help you with rapid response times and advanced technical support</p>
-		</a>
-	</li>
+<ul class="pa-boxes pa-wide">
 	<li>
 		<a href="mailto:customization@pressapps.co">
 			<i class="sk-icon fa fa-code"></i>
 			<h3><?php _e( 'Customization', SK_TEXTDOMAIN ); ?></h3>
-			<p>Do you want to customise our product and don't have the time or skills, get in touch</p>
+			<p><?php _e( 'Do you want to customise our product and don not have the time or skills, get in touch', SK_TEXTDOMAIN ); ?></p>
 		</a>
 	</li>
 </ul>
 
-<div class="pa-cta">
-	<h2 class="pa-subheader"><?php _e( 'Can’t find what you’re looking for?', SK_TEXTDOMAIN ); ?></h2>
-	<p>All our products include six month of standard support answering functionality and setup queries. We reply to queries within 24 hours Monday to Friday.</p>
-	<a class="btn-cta" target="_blank" href="http://pressapps.co/support/"><?php _e( 'Open A Ticket', SK_TEXTDOMAIN ); ?></a>
-</div>
-
 <div class="pa-feedback">
 	<h2 class="pa-subheader"><?php _e( 'Let us know what you think', SK_TEXTDOMAIN ); ?></h2>
-	<p>Your feedback helps us build better products and create better experience for you. Please share you your thoughts and ideas.</p>
+	<p><?php _e( 'Your feedback helps us build better products and create better experience for you. Please share you your thoughts and ideas.', SK_TEXTDOMAIN ); ?></p>
 	<a class="btn-cta" target="_blank" href="http://pressapps.co/feedback/"><?php _e( 'Feedback', SK_TEXTDOMAIN ); ?></a>
 </div>
